844597e41d6d1d6a959d9f3b43187071ee2a7169
Jinmae Jin <jinmae1@gmail.com>
2021-07-24 21:42:52 +0900

    Finish 21.07.23

---
+++ 2021-July/23/src/com/kh/test/multi/dimensional/array/test/PrintPattern.java
@@ -0,0 +1,98 @@
+package com.kh.test.multi.dimensional.array.test;
+
+public class PrintPattern {
+
+	public static void main(String[] args) {
+
+		int N = 5;
+
+		new PrintPattern().pattern1(N);
+		// new PrintPattern().pattern2(N);
+		// new PrintPattern().pattern3(N);
+		// new PrintPattern().pattern4(N);
+		// new PrintPattern().pattern5(N);
+		// new PrintPattern().pattern6(N);
+		// new PrintPattern().pattern7(N);
+		// new PrintPattern().pattern8(N);
+
+	}
+
+	public void pattern1(int N) {
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N - i + 1; j++)
+				System.out.print("*");
+			System.out.println("");
+		}
+	}
+
+	public void pattern2(int N) {
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= i; j++)
+				System.out.print("*");
+			System.out.println("");
+		}
+	}
+
+	public void pattern3(int N) {
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N - i; j++)
+				System.out.print(" ");
+			for (int j = 1; j <= i; j++)
+				System.out.print("*");
+			System.out.println();
+		}
+	}
+
+	public void pattern4(int N) {
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N - i; j++)
+				System.out.print(" ");
+			for (int j = 1; j <= 2 * i - 1; j++)
+				System.out.print("*");
+			System.out.println();
+		}
+	}
+
+	public void pattern5(int N) {
+		for (int i = 1; i <= N; i += 2) {
+			for (int j = 1; j < i; j++)
+				System.out.print(" ");
+			for (int j = 1; j <= N - i + 1; j++)
+				System.out.print("*");
+			System.out.println();
+		}
+	}
+
+	public void pattern6(int N) {
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= 2 * Math.abs(i - (N / 2 + 1)); j++)
+				System.out.print(" ");
+			for (int j = 1; j <= -2 * Math.abs(i - (N / 2 + 1)) + N; j++)
+				System.out.print("*");
+			System.out.println();
+		}
+	}
+
+	public void pattern7(int N) {
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= Math.abs(i - (N / 2 + 1)); j++)
+				System.out.print(" ");
+			for (int j = 1; j <= -2 * Math.abs(i - (N / 2 + 1)) + N; j++)
+				System.out.print("*");
+			System.out.println();
+		}
+	}
+
+	public void pattern8(int N) {
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= -2 * Math.abs(i - (N / 2 + 1)) + N - 1; j++)
+				System.out.print(" ");
+			for (int j = 1; j <= 2 * Math.abs(i - (N / 2 + 1)) + 1; j++)
+				System.out.print("*");
+			System.out.println();
+		}
+	}
+}
---
+++ 2021-July/23/src/com/kh/test/multi/dimensional/array/test/Sample.java
@@ -0,0 +1,72 @@
+package com.kh.test.multi.dimensional.array.test;
+
+import java.util.Random;
+
+public class Sample {
+
+	public static void main(String[] args) {
+
+		new Sample().exercise1();
+		// new Sample().exercise2();
+		// new Sample().exercise3();
+	}
+
+	public void exercise1() {
+
+		Random rand = new Random();
+		int[][] arr = new int[3][5];
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				arr[i][j] = rand.nextInt(100) + 1;
+			}
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			System.out.printf("arr[%d][4]: %d%n", i, arr[i][4]);
+
+		}
+	}
+
+	public void exercise2() {
+
+		Random rand = new Random();
+		int[][] arr = new int[3][4];
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length - 1; j++) {
+				arr[i][j] = rand.nextInt(100) + 1;
+				arr[i][arr[i].length - 1] += arr[i][j];
+			}
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j] + "\t");
+			}
+			System.out.println();
+
+		}
+	}
+
+	public void exercise3() {
+		Random rand = new Random();
+		int[][] arr = new int[4][4];
+
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = 0; j < arr[i].length - 1; j++) {
+				arr[i][j] = rand.nextInt(100) + 1;
+				arr[i][arr[i].length - 1] += arr[i][j];
+				arr[arr.length - 1][j] += arr[i][j];
+			}
+			arr[arr.length - 1][arr[i].length - 1] += arr[i][arr[i].length - 1];
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j] + "\t");
+			}
+			System.out.println();
+		}
+	}
+}
