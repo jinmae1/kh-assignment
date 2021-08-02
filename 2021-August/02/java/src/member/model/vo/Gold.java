176ede8867a6607aca17a25df13e3aa5787cc35b
Jinmae Jin <jinmae1@gmail.com>
2021-08-02 23:47:44 +0900

    Start 21.08.02

---
+++ 2021-August/02/java/src/member/model/vo/Gold.java
@@ -0,0 +1,51 @@
+package member.model.vo;
+
+/**
+ * Gold
+ */
+public class Gold {
+
+    private String name;
+    private String grade;
+    private int point;
+    private static float interest = 0.05f;
+
+    public Gold() {
+
+    }
+
+    public Gold(String name, String grade, int point) {
+        this.name = name;
+        this.grade = grade;
+        this.point = point;
+    }
+
+    public float getInterestPoint() {
+        return point * interest;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGrade() {
+        return this.grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public int getPoint() {
+        return this.point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+}
