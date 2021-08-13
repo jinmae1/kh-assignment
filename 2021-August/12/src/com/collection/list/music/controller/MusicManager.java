b6ff0357d819fa305a88ec91f98cc1abbbcfc8f2
Jinmae Jin <jinmae1@gmail.com>
2021-08-13 12:48:08 +0900

    Finish print and CRUD

--- 2021-August/12/src/com/collection/list/music/controller/MusicManager.java
+++ 2021-August/12/src/com/collection/list/music/controller/MusicManager.java
@@ -8,7 +8,66 @@ import com.collection.list.music.model.vo.Music;
 public class MusicManager {
 	
 	private List<Music> musicList = new ArrayList<>();
-	
+
+	{
+		musicList.add(new Music("Hotel California", "Eagles"));
+		musicList.add(new Music("Let it be", "Beatles"));
+		musicList.add(new Music("Holiday", "Bee Gees"));
+		musicList.add(new Music("Bohemian Rhapsody", "Queen"));
+		musicList.add(new Music("I believe I can fly", "R. Kelly"));
+	}
+
+	public List<Music> selectList() {
+		return musicList;
+	}
+
+	public void addList(Music m) {
+		musicList.add(m);
+	}
+
+	public void addAtZero(Music m) {
+		musicList.add(0, m);
+	}
+
+	public boolean removeMusic(String title) {
+		for (Music music : musicList)
+			if (title.equals(music.getTitle()))
+				return musicList.remove(music);
+
+		return true;
+	}
+
+	public boolean replaceMusic(Music oldMusic, Music newMusic) {
+		int index = musicList.indexOf(oldMusic);
+		if (index > -1)
+			musicList.add(index, newMusic);
+
+		return index > -1;
+
+	}
+
+	public List<Music> searchMusicByTitle(String title) {
+		List<Music> matchList = new ArrayList<>();
+		for (Music music : musicList)
+			if (title.equals(music.getTitle()))
+				matchList.add(music);
+
+		return matchList;
+	}
+
+	public List<Music> searchMusicBySinger(String singer) {
+		List<Music> matchList = new ArrayList<>();
+		for (Music music : musicList)
+			if (singer.equals(music.getSinger()))
+				matchList.add(music);
+
+		return matchList;
+	}
+
+	// public List<Music> orderBy(Comparator<Music> c) {
+	// return new ArrayList<Music>();
+	// }
+
 }
 
 
--- 2021-August/12/src/com/collection/list/music/view/MusicMenu.java
+++ 2021-August/12/src/com/collection/list/music/view/MusicMenu.java
@@ -1,16 +1,22 @@
 package com.collection.list.music.view;
 
 import java.util.List;
+import java.util.Scanner;
 
+import com.collection.list.music.controller.MusicManager;
 import com.collection.list.music.model.vo.Music;
 
 public class MusicMenu {
-	
+
+	private Scanner sc = new Scanner(System.in);
+	private MusicManager manager = new MusicManager();
+
 	public void mainMenu() {
 		String menu = "----- Music Playlist Menu -----\r\n" + 
 					"1.목록보기\r\n" + 
 					"2.마지막에 음악추가\r\n" + 
-					"3.매처음에 음악추가\r\n" + 
+				"3.맨처음에 음악추가\r\n"
+				+ 
 					"4.곡삭제\r\n" + 
 					"5.곡변경\r\n" + 
 					"6.곡명검색\r\n" + 
@@ -19,7 +25,58 @@ public class MusicMenu {
 					"0.종료\r\n" + 
 					"--------------------------------\r\n" + 
 					">> 메뉴선택 : ";
-		
+
+		while (true) {
+			System.out.print(menu);
+			int choice = sc.nextInt();
+
+			switch (choice) {
+				case 1:
+					printList(manager.selectList());
+					break;
+				case 2:
+					sc.nextLine();
+					manager.addList(inputMusic());
+					break;
+				case 3:
+					sc.nextLine();
+					manager.addAtZero(inputMusic());
+					break;
+				case 4:
+					sc.nextLine();
+					if (manager.removeMusic(inputTitle()))
+						System.out.println("## 삭제 성공 ##");
+					else
+						System.out.println("## 삭제 실패 ##");
+					break;
+				case 5:
+
+					sc.nextLine();
+					if (manager.replaceMusic(inputMusic(), inputMusic()))
+						System.out.println("## 수정 성공 ##");
+					else
+						System.out.println("## 수정 실패 ##");
+					break;
+				case 6:
+					sc.nextLine();
+					manager.searchMusicByTitle(inputTitle());
+					break;
+				case 7:
+					sc.nextLine();
+					manager.searchMusicBySinger(inputSinger());
+					break;
+				case 8:
+					orderMenu();
+					break;
+				case 0:
+					return;
+
+				default:
+					System.out.println("잘못 입력하셨습니다.");
+					break;
+			}
+		}
+
 	}
 
 	public void orderMenu() {
@@ -31,22 +88,50 @@ public class MusicMenu {
 										"5. 메인메뉴 돌아가기\r\n" + 
 										"================================================\r\n" + 
 										" >> 메뉴 선택 : ";
-			
+
+		while (true) {
+			System.out.println(orderMenu);
+			int choice = sc.nextInt();
+
+			switch (choice) {
+				case 1:
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+				case 5:
+					break;
+
+				default:
+					break;
+			}
+		}
+
 	}
-	
+
 	public void printList(List<Music> list) {
-		
+		System.out.println();
+		System.out.println("---------- 음악 리스트 ----------");
+		for (Music music : list)
+			System.out.println(music);
+		System.out.println("-------------------------------");
+		System.out.println();
 	}
 	
 	public Music inputMusic() {
-		return null;
+		return new Music(inputTitle(), inputSinger());
 	}
 	
 	public String inputTitle() {
-		return null;
+		System.out.print("> 제목을 입력하세요: ");
+		return sc.nextLine();
 	}
 	
 	public String inputSinger() {
-		return null;
+		System.out.print("> 가수를 입력하세요: ");
+		return sc.nextLine();
 	}
 }
